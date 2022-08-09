3ff18856c852c15f153bbcde17c4e1deed6c55fb
DarioMarret <javier_dario_marret@hotmail.com>
2022-08-09 16:29:20 -0500

    carga principan index

--- src/App.js
+++ src/App.js
@@ -163,8 +163,8 @@ export default function App() {
               )}
               <Routes>
                 {getRoutes(routes)}
-                {/* <Route path="*" element={<Navigate to="/dashboard" />} /> */}
-                <Route path="*" element={<Navigate to="/pagar" />} />
+                <Route path="*" element={<Navigate to="/dashboard" />} />
+                {/* <Route path="*" element={<Navigate to="/pagar" />} /> */}
               </Routes>
             </>
           }
@@ -192,8 +192,8 @@ export default function App() {
             )}
             <Routes>
               {getRoutes(routes)}
-              {/* <Route path="*" element={<Navigate to="/dashboard" />} /> */}
-              <Route path="*" element={<Navigate to="/pagar" />} />
+              <Route path="*" element={<Navigate to="/dashboard" />} />
+              {/* <Route path="*" element={<Navigate to="/pagar" />} /> */}
 
             </Routes>
           </>
